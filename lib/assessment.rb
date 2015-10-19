f572f98e1e749d18868f5ee2e639de58db370cea
Antony Donovan <antony.donovan@generalassemb.ly>
2015-10-19 15:19:30 -0400

    Add example solutions
    Most rely on simple enumerable method invocations

--- lib/assessment.rb
+++ lib/assessment.rb
@@ -5,7 +5,7 @@ class Assessment
   # enumerable supplied.  Assume each entry implements a length
   # method that returns a Fixnum.
   def longest(enum)
-    # Your code goes here
+    enum.max { |a, b| a.length <=> b.length }
   end
 
   # Question 2
@@ -13,36 +13,38 @@ class Assessment
   # enumerable supplied (i.e. apply the `+` operator)
   # Assume each entry implements a `+` method
   def sum(enum)
-    # Your code goes here
+    enum.reduce(&:+)
   end
 
   # Question 3
   # Write ruby code that returns true if some of the entries
   # in the numbers enumerable are odd
   def some_odd(numbers)
-    # Your code goes here
+    numbers.any?(&:odd?)
   end
 
   # Question 4
   # Write ruby code that returns true if every entry in the
   # numbers enumerable are even
   def every_even(numbers)
-    # Your code goes here
+    numbers.all?(&:even?)
   end
 
   # Question 5
   # Write ruby code that returns an array with all of the
   # entries from the words enumerable capitalized.
   def transform(words)
-    # Your code goes here
+    words.map(&:capitalize)
   end
 
   # Question 6
   # Write ruby code to read each line from the file
   # named filename. Concatenate the lines together after
   # replacing the trailing newline character with a
-  # space and return the result.
+  # space.  Remove the trailing space and return the result.
   def read_file(filename)
-    # Your code goes here
+    File.open(filename).each_with_object('') do |line, memo|
+      memo << line.chomp + ' '
+    end.strip
   end
 end
